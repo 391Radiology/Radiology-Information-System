b87551626cc072c61262e220368b95020ad0cbd1
IchigoKIl <rlieu@ualberta.ca>
2015-03-09 17:11:41 -0600

    Troll

---
+++ register.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        <?php
+					echo "Nope, cuz your not Raymond, redirecting...";
+           		header("refresh:3; url=https://www.google.ca/search?client=ubuntu&channel=fs&q=suck+on+that&ie=utf-8&oe=utf-8&gfe_rd=cr&ei=TvX9VJP6OsHDoASupYGIBw");
+        ?>
+    </body>
+</html>

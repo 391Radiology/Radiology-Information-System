ec17e98cc82d13d3ff96e5c1474a1f011173cbd0
Stinn <justin_lu55@hotmail.com>
2015-03-12 19:59:33 -0600

    report

---
+++ report_generating_module.php
@@ -0,0 +1,15 @@
+<?php
+ 	include ("PHPconnectionDB.php");
+ 	$conn=connect();
+ 	//sql 
+	 $sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = “breast cancer” AND r.date > 2014;
+ 	$stid = oci_parse($conn, $sql );
+ 	$res=oci_execute($stid);
+ 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
+		foreach ($row as $item) {
+			echo $item.' &nbsp;';
+		}
+	echo '<br/>';
+ 	}
+?>
+

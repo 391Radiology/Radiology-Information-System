b8f26eec15e8b13bf091a5ec2cf02534e6d60169
Stinn <justin_lu55@hotmail.com>
2015-03-14 18:29:38 -0600

    Update report_generating_module.php

--- report_generating_module.php
+++ report_generating_module.php
@@ -1,8 +1,17 @@
+<?php   // first method    	 
+	if(isset($_POST['validate'])){        	
+		$date=$_POST['Enter the date'];            		
+		$diagnosis=$_POST['Enter the diagnosis'];
+	           	       
+	}	
+?>
+
+
 <?php
  	include ("PHPconnectionDB.php");
  	$conn=connect();
  	//sql 
-	 $sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = “breast cancer” AND r.date > 2014;
+	$sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.date >$date;
  	$stid = oci_parse($conn, $sql );
  	$res=oci_execute($stid);
  	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {

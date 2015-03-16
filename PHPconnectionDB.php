2cc9c8551d91fe618e677ad557f6cbbf5896ebe5
IchigoKIl <rlieu@ualberta.ca>
2015-03-16 17:29:33 -0600

    Update

--- PHPconnectionDB.php
+++ PHPconnectionDB.php
@@ -1,6 +1,6 @@
 <?php
     function connect(){
-        $conn = oci_connect('C##', 'Diablokil9499');
+        $conn = oci_connect('rlieu', 'CMPUT391');
         if (!$conn) {
             $e = oci_error();
             trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);

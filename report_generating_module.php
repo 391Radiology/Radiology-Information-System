e2efbc3afabf34564eac5a94985cef4ed57274f6
Liangrui Lu <liangrui@cs.ualberta.ca>
2015-03-16 19:28:36 -0600

    date

--- report_generating_module.php
+++ report_generating_module.php
@@ -6,14 +6,15 @@
   			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
    		} 	
 	 	//sql 
-		$sql = ‘SELECT first_name, last_name, address, phone, test_date,diagnosis 
+	 	$date = DateTime::createFromFormat('Y-m-j', $date);
+		$sql = 'SELECT first_name, last_name, address, phone, test_date,diagnosis 
 			FROM persons p, radiology_record r 
-			WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.test_date >$date GROUP BY first_name, last_name, address, phone, diagnosis;
+			WHERE p.person_id = r.patient_id AND r.diagnosis = '.$diagnosis.' AND r.test_date >'.$date.' GROUP BY first_name, last_name, address, phone, diagnosis';
 	 	$stid = oci_parse($conn, $sql );
 	 	$res=oci_execute($stid);
 	 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
 			foreach ($row as $item) {
-				echo $item.' &nbsp;';
+				echo $item;
 			}
 			//echo 'Name: '.$row[1].$row[2].'Address'.$row[3].'Phone:'.$row[4]."test date:".row[5].
 			}

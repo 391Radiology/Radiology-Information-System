224c40731d94563a538b275525ad453d49ed6760
Stinn <justin_lu55@hotmail.com>
2015-03-16 18:24:24 -0600

    Update report_generating_module.php

--- report_generating_module.php
+++ report_generating_module.php
@@ -1,20 +1,20 @@
 <?php
- 	
- 	$conn=connect();
- 	if(isset($_POST['validate'])){        	
-		$date=$_POST['Enter the date'];            		
-		$diagnosis=$_POST['Enter the diagnosis'];
-	           	       
-	}	
- 	//sql 
-	$sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.date >$date;
- 	$stid = oci_parse($conn, $sql );
- 	$res=oci_execute($stid);
- 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
-		foreach ($row as $item) {
-			echo $item.' &nbsp;';
-		}
-	echo '<br/>';
+ 	function search_keyword($date,$diagnosis){
+	 	$conn=connect();
+	 	if(isset($_POST['validate'])){        	
+			$date=$_POST['Enter the date'];            		
+			$diagnosis=$_POST['Enter the diagnosis'];
+		           	       
+		}	
+	 	//sql 
+		$sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.date >$date;
+	 	$stid = oci_parse($conn, $sql );
+	 	$res=oci_execute($stid);
+	 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
+			echo 'Name: '.$row[1].$row[2].'Address'.$row[3].'Phone:'.$row[4]."test date:".row[5].
+			}
+		echo '<br/>';
+	 	}
  	}
 ?>
 

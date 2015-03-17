584df8205aa8fb18b4de4a4f0b70288b159376c0
Stinn <justin_lu55@hotmail.com>
2015-03-16 18:36:08 -0600

    report

--- report_generating_module.php
+++ report_generating_module.php
@@ -1,20 +1,50 @@
 <?php
- 	function search_keyword($date,$diagnosis){
+ 	function report_generating($date,$diagnosis){
 	 	$conn=connect();
-	 	if(isset($_POST['validate'])){        	
-			$date=$_POST['Enter the date'];            		
-			$diagnosis=$_POST['Enter the diagnosis'];
-		           	       
-		}	
+			if (!$conn) {
+  			$e = oci_error();
+  			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
+   		} 	
 	 	//sql 
-		$sql = ‘SELECT first_name, last_name, address, phone, test_date FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.date >$date;
+		$sql = ‘SELECT first_name, last_name, address, phone, test_date,diagnosis FROM persons p, radiology_record r WHERE p.person_id = r.patient_id AND r.diagnosis = $diagnosis AND r.test_date >$date GROUP BY first_name, last_name, address, phone, diagnosis;
 	 	$stid = oci_parse($conn, $sql );
 	 	$res=oci_execute($stid);
 	 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
-			echo 'Name: '.$row[1].$row[2].'Address'.$row[3].'Phone:'.$row[4]."test date:".row[5].
+			foreach ($row as $item) {				echo $item.' &nbsp;';			}
+			//echo 'Name: '.$row[1].$row[2].'Address'.$row[3].'Phone:'.$row[4]."test date:".row[5].
 			}
 		echo '<br/>';
 	 	}
  	}
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 

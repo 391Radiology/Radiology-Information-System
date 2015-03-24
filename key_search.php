142fb4be0eab9251a3870c81103912c33c967d69
Omar Zioueche <zioueche@cs.ualberta.ca>
2015-03-23 18:18:31 -0600

    keyword searCHgit add key_search.phpgit add key_search.php

--- key_search.php
+++ key_search.php
@@ -22,7 +22,7 @@
 
 			$sql = 'SELECT 6*(score(1)+score(2))+3*score(3)+score(4) as rank
 			FROM radiology_record r, persons p 
-			WHERE p.person_id = r.patient_id AND contains(first_name,  \''.$keyword.'\''', 1)>0 OR contains(last_name,  \''.$keyword.'\''', 2)>0 OR 			   contains(diagnosis, \''.$keyword.'\''', 3) > 0 OR contains(description,  '\''.$keyword.'\''', 4) > 0 
+			WHERE p.person_id = r.patient_id AND contains(first_name,  \''$keyword.'\', 1)>0 OR contains(last_name,  \''.$keyword.'\', 2)>0 OR 			   contains(diagnosis, \''.$keyword.'\' 3) > 0 OR contains(description,  '\''.$keyword.'\', 4) > 0 
 			ORDER BY (6*(score(1)+score(2))+3*score(3)+score(4))';
 			echo $sql
     }

03d19b914724ce224960f1c0e3713242ac3a04c4
Liangrui Lu <liangrui@cs.ualberta.ca>
2015-03-23 20:09:29 -0600

    report

--- report_generating_module.php
+++ report_generating_module.php
@@ -6,14 +6,11 @@
   			trigger_error(htmlentities($e['message'], ENT_QUOTES), E_USER_ERROR);
    		} 	
 	 	//sql 
-		$sql = 'SELECT persons.first_name, persons.last_name, persons.address, persons.phone, radiology_record.test_date
+		$date = DateTime::createFromFormat('Y-M-J', $date);
+		$sql = 'SELECT persons.first_name, persons.last_name, persons.address, persons.phone, radiology_record.test_date, radiology_record.diagnosis
 			    FROM persons, radiology_record
-			    WHERE persons.person_id = radiology_record.patient_id AND radiology_record.diagnosis = \''.$diagnosis.'\' AND radiology_record.test_date BETWEEN '.$time_period;
-	 	$date = DateTime::createFromFormat('Y-M-J', $date);
-		$sql = 'SELECT';
-		$sql.=' first_name, last_name, address, phone, test_date,diagnosis';
-		$sql.='FROM persons p, radiology_record r ';
-		$sql.='WHERE p.person_id = r.patient_id AND r.diagnosis = '.$diagnosis.' AND r.test_date >'.date_format($date,"J-M-Y");
+			    WHERE persons.person_id = radiology_record.patient_id AND radiology_record.diagnosis = \''.$diagnosis.'\' AND radiology_record.test_date > '.date_format($date,"J-M-Y");
+	
 		$sql.= 'GROUP BY first_name, last_name, address, phone, diagnosis';
 	 	$stid = oci_parse($conn, $sql );
 	 	$res=oci_execute($stid);

5fae8502427ddab7925cb9d12dc39f818c5a21ac
IchigoKIl <rlieu@ualberta.ca>
2015-03-23 19:36:09 -0600

    Updated

--- account.php
+++ account.php
@@ -1,6 +1,5 @@
 <?php
 	include('PHPconnectionDB.php');
-	include('medical_info.php');
 	include("key_search.php");
 	include("forms.php");
 	session_start();
@@ -10,20 +9,8 @@
 	<body>
 		<?php
 				if (isset($_SESSION["usr"])) {
-					//establish types array
-					$types = array('a' => 'Admin',
-								'd' => 'Doctor',
-								'r' => 'Radiologist',
-								'p' => 'Patient');
-
 					user_info($_SESSION['pid']);
 					search_form();
-
-					if (isset($_GET['search'])) {
-						$search = implode(" ", $_GET["key"]);
-						echo 'Search results for: '.$search.' <br>';
-						search_keyword("hi", null, null);
-					}
 				}
 				else {
 					echo "Session not found, please login";
--- forms.php
+++ forms.php
@@ -1,5 +1,11 @@
 <?php
     function user_info($pid) {
+			//establish types array
+			$types = array('a' => 'Admin',
+								'd' => 'Doctor',
+								'r' => 'Radiologist',
+								'p' => 'Patient');
+								
         //establish connection
         $conn = connect();
         if (!$conn) {
@@ -38,24 +44,25 @@
     	<form name="search" method="get" action="account.php">
 			Start Date : <input type="date" placeholder="yyyy-mm-dd" maxlength=10 
 			<?php
-       		if (isset($_POST['sdate']) and DateTime::createFromFormat('Y-m-j', $_POST['sdate'])) {
-					echo 'value=', $_POST['sdate'];
+       		if (isset($_GET['sdate']) and DateTime::createFromFormat('Y-m-j', $_GET['sdate'])) {
+					echo 'value=', $_GET['sdate'];
 				}
 			?> name="sdate"/>
 			End Date : <input type="date" placeholder="yyyy-mm-dd" maxlength=10 max="9999-12-31"
 			<?php
-				if (isset($_POST['edate']) and DateTime::createFromFormat('Y-m-j', $_POST['edate'])) {
-					echo 'value=', $_POST['edate'];
+				if (isset($_GET['edate']) and DateTime::createFromFormat('Y-m-j', $_GET['edate'])) {
+					echo 'value=', $_GET['edate'];
 				}
 			?> name="edate"/>
 			<input type="submit" name="search" value="Search"/> <br>
 			<div id="keyword_list">
 				<?php
-					$ni = "hi";
 					for($n=0; $n < max(1, count($_GET["key"])); $n++) {
-						?>
-							<input type="text" name="key[]" <?php echo 'value=\''.$_GET["key"][$n].'\''; ?>/><br>
-						<?php
+						if(!isset($_GET["key"]) or $_GET["key"][$n] or ($n == 0 and count(array_filter($_GET["key"])) == 0)) {
+							?>
+								<input type="text" name="key[]" <?php echo 'value=\''.$_GET["key"][$n].'\''; ?>/><br>
+							<?php
+						}
 					}
 				?>
 			</div>
@@ -63,6 +70,18 @@
                         
 		</form>
     <?php
+    	if (isset($_GET['search'])) {
+			$search = implode(" ", $_GET["key"]);
+			if ($search) { 
+				echo 'Search results for: '.$search.' <br>';
+			} else {
+				?>
+					<div style="color:red;">
+						Please input some search information <br>
+					</div>	
+				<?php
+			}
+		}
     }
 ?>
 

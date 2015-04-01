0598e7ee1f087086de68a9da1fc780d488a1b6b7
Liangrui Lu <liangrui@cs.ualberta.ca>
2015-04-01 00:08:27 -0600

    analysis

--- data_analysis_module.php
+++ data_analysis_module.php
@@ -1,6 +1,6 @@
 <?php
 	//$date has three options:"Weekly", "Monthly" or "Weekly"
- 	function data_analysis($patient,$sdate,$edate,$test_type){
+ 	function data_analysis($fname,$lname,$test_type,$sdate,$edate){
 	 	$conn=connect();
 			if (!$conn) {
   			$e = oci_error();
@@ -9,18 +9,92 @@
 
 		//Create a Table to get all the information that useful to data_analysis
 		
-		$sql = 'CREATE Information_for_data_analysis AS 
-                SELECT p.FIRST_NAME, p.LAST_NAME, r.TEST_TYPE, r.TEST_DATE, i.IMAGE_ID 
-		FROM PERSONS p, RADIOLOGY_RECORD r, PACS_IMAGES i
-                WHERE P.PERSON_ID = R.PATIENT_ID AND I.RECORD_ID = R.RECORD_ID';
+		$sql = 'CREATE OR REPLACE VIEW Information_for_data_analysis ( FIRST_NAME, LAST_NAME, TEST_TYPE, TEST_DATE ,image_count) AS 
+                SELECT p.FIRST_NAME, p.LAST_NAME, r.TEST_TYPE, r.TEST_DATE ,count(i.IMAGE_ID)
+					 FROM PERSONS p, RADIOLOGY_RECORD r, PACS_IMAGES i
+                WHERE p.PERSON_ID = r.PATIENT_ID AND i.RECORD_ID = r.RECORD_ID 
+                GROUP BY p.FIRST_NAME, p.LAST_NAME, r.TEST_TYPE, r.TEST_DATE';
                 
 		$stid = oci_parse($conn, $sql);
 
 		$res = oci_execute($stid);
+		
+		
+		
+
+
+      // Sql command
+		$sql = 'SELECT i.FIRST_NAME,i.LAST_NAME,i.TEST_TYPE,i.TEST_DATE,i.image_count
+			    FROM Information_for_data_analysis i
+			    WHERE LOWER(i.TEST_TYPE) = \''.strtolower($test_type).'\' ';
+
+		// Prepare sql using conn and returns the statement identifier
+        $stid = oci_parse($conn, $sql);
+
+        // Execute a statement returned from oci_parse()
+        $res = oci_execute($stid);
+	 	
+	 	  if (!$res) {
+        	// Error, retrieve the error using the oci_error() function & output an error message
+     	   	$err = oci_error($stid);
+     	   	echo htmlentities($err['message']);
+        } else {
+        	// No error
+        	// Fetch and output info
+        	echo 'Results for:<br>Diagnosis: '.$diagnosis.'<br>Start Date: '.$sdate.' End Date: '.$edate.'<br>';
+        	if ($info = oci_fetch_array($stid)) {
+        	?>
+        		<table border="1">
+        		<th width="100" align="center" valign="middle">First Name</th>
+        		<th width="100" align="center" valign="middle">Last Name</th>
+        		<th width="100" align="center" valign="middle">Test Type</th>
+        		<th width="100" align="center" valign="middle">Test Date</th>
+        		<th width="100" align="center" valign="middle">Image Number</th>
+        	<?php
+				while ($info) {
+				?>
+				 	<tr>
+						<td><?php echo $info["FIRST_NAME"]; ?></td>	
+						<td><?php echo $info["LAST_NAME"]; ?></td>
+						<td><?php echo $info["TEST_TYPE"]; ?></td>
+						<td><?php echo $info["TEST_DATE"]; ?></td>	
+						<td><?php echo $info["IMAGE_COUNT"]; ?></td>
+					</tr>
+	 			<?php
+	 				$info = oci_fetch_array($stid);
+        		}
+        	?>
+				</table>
+			<?php
+        	} else {
+        		// Error message for having no matching results
+			?>
+				<div style="color:red;">
+					No matching results
+				</div>		
+			<?php
+        	}
+
+	 	}
+	 	/*
+		
+		
+		$sql = 'SELECT * FROM Information_for_data_analysis I WHERE LOWER(I.FIRST_NAME) = \''.strtolower($fname).'\'  ';
+		
+	   $stid = oci_parse($conn, $sql);
+
+		$res = oci_execute($stid);
+	 	while (($row = oci_fetch_array($stid, OCI_ASSOC))) {
+			foreach ($row as $item) {
+				echo '<td>';
+				echo $item;
+				echo '</td>';
+			}
+				}
 
 	//Create sql part by part
 	
-	 	
+	 	/*
 		$sql = 'SELECT';
 		//set up which factor to choose
 		if(!empty($patient)){
@@ -38,18 +112,7 @@
 			if ($date == "Weekly"){
 				$sql = $sql.'TRUNC(TEST_DATE,\'IW\'),)';
 				}
-			/*
-			if ($date == "Yearly"){
-				$sql = $sql.'to_char(TEST_DATE,'IW'),)';
-				}
-			if ($date == "Monthly"){
-				$sql = $sql.'to_char(TEST_DATE,'M'),)';
-				}
-			if ($date == "Weekly"){
-				$sql = $sql.'to_char(TEST_DATE,'Y'),)';
-				}
-				*/
-			
+
 			echo '<th> Date </th>';
 
 		}
@@ -74,17 +137,7 @@
 			if ($date == "Weekly"){
 				$sql = $sql.'TRUNC(TEST_DATE,\'IW\'),)';
 				}
-			/*
-			if ($date == "Yearly"){
-				$sql = $sql.'to_char(TEST_DATE,'IW'),)';
-				}
-			if ($date == "Monthly"){
-				$sql = $sql.'to_char(TEST_DATE,'M'),)';
-				}
-			if ($date == "Weekly"){
-				$sql = $sql.'to_char(TEST_DATE,'Y'),)';
-				}
-				*/
+
 			
 
 			//$sql = $sql.'TEST_DATE';
@@ -109,11 +162,12 @@
 			
 			
 			}
+			*/
 		echo '<br/>';
 		oci_free_statement($stid);
 		oci_close($conn);
 	 	}
- 	}
+ 	
 ?>
 
 
